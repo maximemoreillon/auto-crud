caadd37a3f2de27be07070fb62dda1e7fe2f48a2
Maxime Moreillon <m.moreillon@gmail.com>
2025-03-21 11:59:18 +0900

    OIDC

--- server.ts
+++ server.ts
@@ -1,47 +1,50 @@
 // This file is used in the Docker container
-import dotenv from "dotenv"
-dotenv.config()
+import "dotenv/config";
 
-import express from "express"
-import "express-async-errors"
+import express from "express";
+import "express-async-errors";
 
-import cors from "cors"
-import prismaAutoCrud from "./index"
-import prismaClient from "./prismaClient"
-import auth from "@moreillon/express_identification_middleware"
-import swaggerUi from "swagger-ui-express"
-import swaggerDocument from "./swagger-output.json"
-import { Response } from "express"
-import { version, author } from "./package.json"
+import cors from "cors";
+import prismaAutoCrud from "./index";
+import prismaClient from "./prismaClient";
+import authMiddleware from "@moreillon/express-oidc";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger-output.json";
+import { Response } from "express";
+import { version, author } from "./package.json";
 
-console.log(`Auto CRUD v${version}`)
+console.log(`Auto CRUD v${version}`);
 
-const { PORT = 80, READ_ONLY, IDENTIFICATION_URL } = process.env
+const { PORT = 80, READ_ONLY, OIDC_JWKS_URI } = process.env;
 
 const options = {
   readonly: !!READ_ONLY,
-}
+};
 
-export const app = express()
-app.use(cors())
-app.use(express.json())
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+export const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.get("/", (_, res: Response) => {
   res.send({
     application: "Auto CRUD",
     version,
     author,
     options,
-    identifcation_url: IDENTIFICATION_URL,
-  })
-})
+    oidc_jwks_uri: OIDC_JWKS_URI,
+  });
+});
 
-if (IDENTIFICATION_URL) {
-  console.log(`Authentication enabled. URL: ${IDENTIFICATION_URL}`)
-  app.use(auth({ url: IDENTIFICATION_URL }))
+if (OIDC_JWKS_URI) {
+  console.log(`Authentication enabled. JWKS URI: ${OIDC_JWKS_URI}`);
+  app.use(
+    authMiddleware({
+      jwksUri: OIDC_JWKS_URI,
+    })
+  );
 }
 
-app.use(prismaAutoCrud(prismaClient, options))
+app.use(prismaAutoCrud(prismaClient, options));
 app.listen(PORT, () => {
-  console.log(`Express Listening on port ${PORT}`)
-})
+  console.log(`Express Listening on port ${PORT}`);
+});
